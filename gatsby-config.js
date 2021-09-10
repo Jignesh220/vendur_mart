ee80d82a1f80894184c7cbf4783b4602f458af8b
Jignesh Baria <71313366+Jignesh220@users.noreply.github.com>
2021-09-10 11:42:15 +0530

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "vendur_mart",
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+  ],
+};
