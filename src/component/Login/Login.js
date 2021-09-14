f637df0132cd0befa4e4b84592ad4297d828458e
Jignesh Baria <71313366+Jignesh220@users.noreply.github.com>
2021-09-14 13:59:02 +0530

    validation

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,16 +1,73 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import "./login.css";
-import { GoogleAuthProvider } from "firebase/auth";
 import LoginSvg from "../../images/login.svg";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signInWithCredential, GoogleAuthProvider } from "firebase/auth";
 
-const Login = () => {
-  const signin = () => {
-    getAuth.signInWithPopup(GoogleAuthProvider).catch(alert);
-  };
 
-  return (
-    <section class="vh-100">
+
+class login extends Component{
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fields: {},
+      errors: {},
+    };
+  }
+
+  handleValidation() {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    //Email
+    if (!fields["email"]) {
+      formIsValid = false;
+      errors["email"] = "Cannot be empty";
+    }
+
+    if (typeof fields["email"] !== "undefined") {
+      let lastAtPos = fields["email"].lastIndexOf("@");
+      let lastDotPos = fields["email"].lastIndexOf(".");
+
+      if (
+        !(
+          lastAtPos < lastDotPos &&
+          lastAtPos > 0 &&
+          fields["email"].indexOf("@@") == -1 &&
+          lastDotPos > 2 &&
+          fields["email"].length - lastDotPos > 2
+        )
+      ) {
+        formIsValid = false;
+        errors["email"] = "Email is not valid";
+      }
+    }
+
+    this.setState({ errors: errors });
+    return formIsValid;
+  }
+
+  contactSubmit(e) {
+    e.preventDefault();
+
+    if (this.handleValidation()) {
+      alert("Form submitted");
+    } else {
+      alert("Form has errors.");
+    }
+  }
+
+  handleChange(field, e) {
+    let fields = this.state.fields;
+    fields[field] = e.target.value;
+    this.setState({ fields });
+  }
+
+  render() {
+    return (
+      <section class="vh-100">
       <div class="container py-5 h-100">
         <div class="row d-flex align-items-center justify-content-center h-100">
           <div class="col-4 col-md-8 col-lg-7 col-xl-6">
@@ -25,11 +82,14 @@ const Login = () => {
               <div class="form-outline mb-4">
                 <input
                   type="email"
-                  id="form1Example13"
+                  id="emailid"
                   class="form-control form-control-lg"
                   placeholder="E-mail Address"
+                  onChange={this.handleChange.bind(this, "email")}
+                  value={this.state.fields["email"]}
+                  required
                 />
-                <label class="form-label" for="form1Example13">
+                <label class="form-label" for="emailid">
                   Email address
                 </label>
               </div>
@@ -37,11 +97,14 @@ const Login = () => {
               <div class="form-outline mb-4">
                 <input
                   type="password"
-                  id="form1Example23"
+                  id="Password"
                   class="form-control form-control-lg"
                   placeholder="Password"
+                  onChange={this.handleChange.bind(this, "Password")}
+                  value={this.state.fields["Password"]}
+                  required required minLength={6}
                 />
-                <label class="form-label" for="form1Example23">
+                <label class="form-label" for="Password">
                   Password
                 </label>
               </div>
@@ -52,10 +115,10 @@ const Login = () => {
                     class="form-check-input"
                     type="checkbox"
                     value=""
-                    id="form1Example3"
+                    id="submitbutton"
                     checked
                   />
-                  <label class="form-check-label" for="form1Example3">
+                  <label class="form-check-label" for="submitbutton">
                     {" "}
                     Remember me{" "}
                   </label>
@@ -75,8 +138,9 @@ const Login = () => {
                 class="btn btn-primary btn-lg btn-block loginwithgoogle"
                 href="#"
                 role="button"
+                onClick={onSignIn}
               >
-                <i class="fab fa-facebook-f me-2" onClick={signin}></i>Continue
+                <i class="fab fa-google me-2"></i>Continue
                 with Google
               </a>
               <a
@@ -91,7 +155,8 @@ const Login = () => {
         </div>
       </div>
     </section>
-  );
-};
+    )
+  }
+}
 
-export default Login;
+export default login;
